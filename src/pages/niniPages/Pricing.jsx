ac822b30387cbc548f50f0beb6073735bb4319f2
rodin <rodinwitman9@gmail.com>
2026-04-08 13:29:34 +0300

    Pricing cards

--- src/pages/niniPages/Pricing.jsx
+++ src/pages/niniPages/Pricing.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaCheckCircle } from "react-icons/fa";
 
 const Pricing = () => {
   return (
      <>
     {/* Section Hero et Banière */}
-    <section className='relative w-[1700px] h-[450px] mx-1  py-24 px-120 flex flex-col gap-16 '>
+    <section className='relative w-[1680px] h-[450px] mx-1  py-24 px-120 flex flex-col gap-16 '>
       <div 
         className="absolute inset-0 bg-[url('/pricingCoach4.png')] bg-cover bg-center"
         style={{ opacity: 0.2 }} 
@@ -26,29 +27,58 @@ const Pricing = () => {
     </section>
 
     {/* Pricing card section */}
-    <section className='text-white'>
-      <div className=''>
-        <ul className='flex flex-row gap-10'>
-          <li>
-            <div>
-              <h1>STARTER</h1> 
-              <div>
-                <h1>$</h1>
-                <h1>49</h1>
-                <h2>/MO</h2>
+    <section className='w-[1280px] h-[680px] text-white '>
+      <div className='py-[80px] px-[32px] position relative left-[200px] bg-[#131313]'>
+        <ul className='w-[1216px] h-[520px] max-w-[1280px] grid grid-rows-1 grid-cols-3 gap-y-8 gap-x-20 opacity-100'>
+          <li className='w-[384px] h-[525px] flex justify-between p-10 border-1 border-olive-600 opacity-100 col-start-1 row-start-1 col-span-1 row-span-1'>
+            <div className="">
+              <h1 className="w-[302px] h-[32px] mb-2.5 font-bold text-[24px] leading-[32px] tracking-[-1.2px] align-middle uppercase">STARTER</h1> 
+              <div className='flex flex-row gap-2'>
+                <h1 class="w-[23px] h-[42px] position relative top-[52px] font-['Space_Grotesk'] font-black text-[36px] leading-[40px] tracking-normal align-middle">$</h1>
+                <h1 className="w-[123px] h-[101px] position relative right-[10px] font-black text-[96px] leading-[96px] tracking-normal align-middle">49</h1>
+                <h2 className="w-[33px] h-[25px] position relative top-[67px] right-[15px] font-['Space_Grotesk'] font-bold text-[16px] leading-[24px] tracking-normal align-middle">/MO</h2>
               </div>   
+              <ul className='w-[319px] h-[160px] pt-[8px] mt-[32px] flex flex-col gap-[32px]'>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Access to gym floor</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Basic locker access</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>1 Orientation session</li>
+              </ul>
+              <button className="w-[302px] h-[60px] border-2 mt-[48px] border-[#FFFFFF]">SELECT PLAN</button>
 
             </div>
           </li>
-          <li>
+          <li className='w-[404px] h-[610px] bg-[#E8000D] shadow-[0_0_40px_0_#E8000D40] flex justify-between p-10 opacity-100 col-start-2 row-start-1 col-span-1 row-span-1 left-[416px]'>
             <fieldset>
-              <legend>MOST POPULAR</legend>
-              <h1>PRO</h1>
+              <legend className="w-[158px] py-2 text-center position relative bottom-[60px] bg-[#000000]">MOST POPULAR</legend>
+              <h1 className="w-[302px] h-[32px] font-bold text-[24px] leading-[32px] tracking-[-1.2px] align-middle uppercase position relative bottom-[40px]">PRO</h1>
+              <div className='flex flex-row gap-2'>
+                <h1 class="w-[23px] h-[42px] position relative top-[52px] font-['Space_Grotesk'] font-black text-[36px] leading-[40px] tracking-normal align-middle">$</h1>
+                <h1 className="w-[123px] h-[101px] position relative right-[10px] font-black text-[96px] leading-[96px] tracking-normal align-middle">89</h1>
+                <h2 className="w-[33px] h-[25px] position relative top-[67px] right-[15px] font-['Space_Grotesk'] font-bold text-[16px] leading-[24px] tracking-normal align-middle">/MO</h2>
+              </div>   
+              <ul className='w-[319px] h-[160px] pt-[8px] mt-[32px] flex flex-col gap-[32px]'>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Everything in Starter</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Unlimited group classes</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Recovery zone access</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Nutrition guide</li>
+              </ul>
+              <button className="w-[319px] h-[67px] py-[20px] px-[24px] mt-[96px] bg-[#FFFFFF]"><p className=" h-[25px] text-[16px] leading-[24px] tracking-[-0.8px] text-center text-[#E8000D] align-middle uppercase font-bold">CHOOSE PRO</p></button>
             </fieldset>
           </li>
-          <li>
+          <li className='w-[384px] h-[525px] flex justify-between p-10 border-1 border-olive-600 opacity-100 col-start-3 row-start-1 col-span-1 row-span-1'>
             <div>
-              <h1>ELITE</h1>
+              <h1 className="w-[302px] h-[32px] mb-2.5 font-bold text-[24px] leading-[32px] tracking-[-1.2px] align-middle uppercase">ELITE</h1>
+              <div className='flex flex-row gap-2'>
+                <h1 class="w-[23px] h-[42px] position relative top-[52px] font-['Space_Grotesk'] font-black text-[36px] leading-[40px] tracking-normal align-middle">$</h1>
+                <h1 className="w-[123px] h-[101px] position relative right-[10px] font-black text-[96px] leading-[96px] tracking-normal align-middle">149</h1>
+                <h2 className="w-[33px] h-[25px] position relative top-[67px] left-[30px] font-['Space_Grotesk'] font-bold text-[16px] leading-[24px] tracking-normal align-middle">/MO</h2>
+              </div>   
+              <ul className='w-[319px] h-[160px] pt-[8px] mt-[32px] flex flex-col gap-[32px]'>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Everything in pro</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Personnal Coach (2/mo)</li>
+                <li className='flex flex-row gap-2 text-xl'><FaCheckCircle className='position relative top-1'/>Monthly body scans</li>
+              </ul>
+              <button className="w-[302px] h-[60px] border-2 mt-[48px] border-[#FFFFFF]">SELECT PLAN</button>
             </div>            
           </li>
         </ul>
